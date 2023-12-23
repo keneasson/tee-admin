e03fe6c1da18eddf62a726158ad19bbd2eb09539
Ken Easson <ken.easson@rangle.io>
2023-12-23 15:44:57 -0500

    Schedules v1 working

--- packages/app/features/schedules/memorial.tsx
+++ packages/app/features/schedules/memorial.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, XStack, YStack } from 'tamagui'
 import { Text } from '@my/ui'
 import { MemorialService } from 'app/features/schedules/schedules-screen'
+import { monthDay } from 'app/provider/date-utils'
 
 export const Memorial: React.FC<{
   schedule: MemorialService[]
@@ -10,6 +11,35 @@ export const Memorial: React.FC<{
 
   return (
     <YStack>
+      <YStack
+        key={'header'}
+        borderColor={'lightGrey'}
+        borderWidth={1}
+        backgroundColor={'cornflowerblue'}
+      >
+        <XStack padding={'$2'}>
+          <View flex={1} flexBasis={0}>
+            <Text fontWeight={'bold'}>Date</Text>
+          </View>
+          <View flex={1} flexBasis={0}>
+            <Text fontWeight={'bold'}>Preside</Text>
+          </View>
+          <View flex={1} flexBasis={0}>
+            <Text fontWeight={'bold'}>Exhort</Text>
+          </View>
+          <View flex={1} flexBasis={0}>
+            <Text fontWeight={'bold'}>Steward</Text>
+          </View>
+          <View flex={1} flexBasis={0}>
+            <Text fontWeight={'bold'}>Doorkeeper</Text>
+          </View>
+        </XStack>
+        <XStack padding={'$2'}>
+          <YStack flex={6}>
+            <Text fontWeight={'bold'}>Fellowship Lunches / Activities</Text>
+          </YStack>
+        </XStack>
+      </YStack>
       {schedule.map((service: MemorialService, index) => {
         const date = new Date(service.Date)
         const bgColour = date < today ? '#ccc6c6' : 'white'
@@ -18,9 +48,7 @@ export const Memorial: React.FC<{
           <YStack key={index} borderColor={'lightGrey'} borderWidth={1} backgroundColor={bgColour}>
             <XStack padding={'$2'}>
               <View flex={1} flexBasis={0}>
-                <Text fontStyle={'normal'}>{`${date.toLocaleString('default', {
-                  month: 'short',
-                })} ${date.getDate()}`}</Text>
+                <Text>{monthDay(date)}</Text>
               </View>
               <View flex={1} flexBasis={0}>
                 <Text>{service.Preside}</Text>
---
+++ packages/app/provider/date-utils.ts
@@ -0,0 +1,5 @@
+export const monthDay = (date: Date) => {
+  return `${date.toLocaleString('default', {
+    month: 'short',
+  })} ${date.getUTCDate()}`
+}
