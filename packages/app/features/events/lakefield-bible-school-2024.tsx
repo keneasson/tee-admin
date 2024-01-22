cee07153250c2518f96a236f2920e34ac1ac1784
Ken Easson <ken.easson@rangle.io>
2024-01-22 11:40:44 -0500

    add Lakefield-bible-school-2024 to Events - manually

---
+++ packages/app/features/events/lakefield-bible-school-2024.tsx
@@ -0,0 +1,15 @@
+import { Paragraph, ProgramElement } from "@my/ui"
+
+
+export const LakefieldBibleSchool2024: React.FC = () => {
+  return (
+<ProgramElement>August 5-10, 2024 – Lakefield Christadelphian Bible School at Fleming</ProgramElement>
+  <Paragraph>Location: same as 2023, Fleming College in Peterborough, Ontario</Paragraph>
+  <Paragraph>Dates: Monday August 5th to Saturday August 10th, 2024</Paragraph>
+  <Paragraph>Speakers</Paragraph>
+  <Paragraph>Bro. Mark Whittaker</Paragraph>
+  <Paragraph>Bro. Bill Link</Paragraph>
+  <Paragraph>Registration: To be open in March</Paragraph>
+
+  <Paragraph>Details and Flyer coming soon.</Paragraph>)
+    }
