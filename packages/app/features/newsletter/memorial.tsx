6b055576fd3eaefeaefbf72921559b2f5d89d093
Ken Easson <ken.easson@rangle.io>
2025-07-04 22:28:27 -0400

    polished navigation phase 1 done.

--- packages/app/features/newsletter/memorial.tsx
+++ packages/app/features/newsletter/memorial.tsx
@@ -25,33 +25,38 @@ export const NextMemorial: React.FC<NextMemorialProps> = ({ event, isSameDay }) 
   }
   return (
     <Section>
-      {!isSameDay && (
+      {!isSameDay ? (
         <Paragraph size={'$5'} fontWeight={600}>
           {event.Date.toString()}
         </Paragraph>
-      )}
+      ) : null}
       <Paragraph size={'$5'} fontWeight={600}>
         Memorial Service
       </Paragraph>
       <XStack $xs={{ flexDirection: 'column' }}>
         <YStack flexGrow={1}>
           <Paragraph>
-            <Text fontWeight={600}>Presiding:</Text> {event.Preside}
+            <Text fontWeight={600}>Presiding: </Text>
+            <Text>{event.Preside}</Text>
           </Paragraph>
           <Paragraph>
-            <Text fontWeight={600}>Exhorting:</Text> {event.Exhort}
+            <Text fontWeight={600}>Exhorting: </Text>
+            <Text>{event.Exhort}</Text>
           </Paragraph>
           <Paragraph>
-            <Text fontWeight={600}>Organist:</Text> {event.Organist}
+            <Text fontWeight={600}>Organist: </Text>
+            <Text>{event.Organist}</Text>
           </Paragraph>
           <Paragraph>
-            <Text fontWeight={600}>Steward:</Text> {event.Steward}
+            <Text fontWeight={600}>Steward: </Text>
+            <Text>{event.Steward}</Text>
           </Paragraph>
           <Paragraph>
-            <Text fontWeight={600}>Doorkeeper:</Text> {event.Doorkeeper}
+            <Text fontWeight={600}>Doorkeeper: </Text>
+            <Text>{event.Doorkeeper}</Text>
           </Paragraph>
         </YStack>
-        {event['Hymn-opening'] && (
+        {event['Hymn-opening'] ? (
           <YStack flexGrow={1}>
             <Paragraph fontWeight={600}>Hymns:</Paragraph>
             <Paragraph>{event['Hymn-opening']}</Paragraph>
@@ -59,18 +64,18 @@ export const NextMemorial: React.FC<NextMemorialProps> = ({ event, isSameDay }) 
             <Paragraph>{event['Hymn-memorial']}</Paragraph>
             <Paragraph>{event['Hymn-closing']}</Paragraph>
           </YStack>
-        )}
+        ) : null}
       </XStack>
-      {event.Collection && <Paragraph>Second Collection is for {event.Collection}</Paragraph>}
-      {event.Lunch && <Paragraph>Lunch will be held at the hall</Paragraph>}
-      {event.Activities && <Paragraph>{event.Activities}</Paragraph>}
+      {event.Collection ? <Paragraph>Second Collection is for {event.Collection}</Paragraph> : null}
+      {event.Lunch ? <Paragraph>Lunch will be held at the hall</Paragraph> : null}
+      {event.Activities ? <Paragraph>{event.Activities}</Paragraph> : null}
       <Separator alignSelf="stretch" borderColor={'$light4grey'} />
       <Accordion overflow="hidden" type="multiple">
         <Accordion.Item value="a1">
           <Accordion.Trigger flexDirection="row" justifyContent="space-between">
             {({ open }) => (
               <>
-                <Paragraph>Zoom Info - Click to open</Paragraph>
+                <Text>Zoom Info - Click to open</Text>
                 <Square animation="quick" rotate={open ? '180deg' : '0deg'}>
                   <ChevronDown size="$1" />
                 </Square>
@@ -89,18 +94,18 @@ export const NextMemorial: React.FC<NextMemorialProps> = ({ event, isSameDay }) 
             <Paragraph>
               <ExtLink href="tel:+14388097799,,586952386#,,,,*036110# Canada">
                 <Text>+14388097799,,586952386#,,,,*036110#</Text>
-              </ExtLink>{' '}
-              Canada
+              </ExtLink>
+              <Text> Canada</Text>
             </Paragraph>
             <Paragraph>
               <ExtLink href="tel:+15873281099,,586952386#,,,,*036110# Canada">
                 <Text>+15873281099,,586952386#,,,,*036110#</Text>
-              </ExtLink>{' '}
-              Canada
+              </ExtLink>
+              <Text> Canada</Text>
             </Paragraph>
             <Separator alignSelf="stretch" borderColor={'$borderColor'} />
             <Paragraph>
-              Find your local number:{' '}
+              <Text>Find your local number: </Text>
               <ExtLink href="https://us02web.zoom.us/u/kc1iqj9IRk">
                 <Text>https://us02web.zoom.us/u/kc1iqj9IRk</Text>
               </ExtLink>
@@ -112,7 +117,7 @@ export const NextMemorial: React.FC<NextMemorialProps> = ({ event, isSameDay }) 
           <Accordion.Trigger flexDirection="row" justifyContent="space-between">
             {({ open }) => (
               <>
-                <Paragraph>YouTube Info - click to open</Paragraph>
+                <Text>YouTube Info - click to open</Text>
                 <Square animation="quick" rotate={open ? '180deg' : '0deg'}>
                   <ChevronDown size="$1" />
                 </Square>
@@ -122,15 +127,15 @@ export const NextMemorial: React.FC<NextMemorialProps> = ({ event, isSameDay }) 
           <Accordion.Content>
             <Paragraph fontWeight={600}>Watch on YouTube</Paragraph>
             <Paragraph>
-              YouTube: <ExtLink href={event.YouTube}>{event.YouTube}</ExtLink>
+              <Text>YouTube: </Text>
+              <ExtLink href={event.YouTube}>{event.YouTube}</ExtLink>
             </Paragraph>
             <Paragraph>
-              Previous recordings are available on the Toronto East Christadelphians YouTube channel
-              here:{' '}
+              <Text>Previous recordings are available on the Toronto East Christadelphians YouTube channel here: </Text>
               <ExtLink href="https://www.youtube.com/channel/UCyJamaI5mQImCF8hWE7Yp-w">
                 <Text>https://www.youtube.com/channel/UCyJamaI5mQImCF8hWE7Yp-w</Text>
-              </ExtLink>{' '}
-              The videos can be under either Upload or Live
+              </ExtLink>
+              <Text> The videos can be under either Upload or Live</Text>
             </Paragraph>
           </Accordion.Content>
         </Accordion.Item>
